96a674b1129b21f5fcb8114ef915fb9233c40b4a
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-11 19:54:48 +0530

    updated code

--- Step 1 Learn the basics/stl.cpp
+++ Step 1 Learn the basics/stl.cpp
@@ -29,7 +29,7 @@ void vect()
         cout << v1[i] << " ";
     }
     cout << endl;
-    
+
     vector<int>::iterator it = v1.begin();
     it++;
     cout<<*(it);
@@ -51,3 +51,121 @@ void vect()
     }
 }
 
+// list : the difference to vector is that it allows to push at front also the only difference
+#include<list>
+void list_example(){
+  list<int> li;
+  li.push_back(3);
+  li.push_back(2);
+  li.push_front(1);
+  li.push_front(0);
+}
+
+// dequeue : similar to list and vector, all operations are generally same
+void dequeue_example(){
+  deque<int> d;
+  d.push_back(2);
+  d.push_front(1);
+  d.pop_back();
+  d.pop_front();
+}
+
+// stack
+#include<stack>
+void stack_example(){
+  stack<int> st;
+  st.push(44);
+  st.size();
+  st.top();
+  st.pop();
+}
+
+
+// Queue
+// #include<queue>
+void queue_example(){
+  queue<int> q;
+  q.push(10);
+  q.front();
+  q.back();
+  q.pop();
+}
+
+// priority Queue
+void priority_queue(){
+  priority_queue<int> pq; // Maxheap
+  pq.push(10);
+  pq.back();
+  pq.top();
+  
+  priority_queue<int, vector<int> greater<int>> minheap; // this is MinHeap
+
+}
+
+// set
+#include<set>
+void set_example(){
+  set<int> st;
+  st.insert(22);
+  st.insert(21);
+  st.insert(23);
+  st.insert(24);
+
+  st.count(22); 
+  auto it1 = st.find(21);
+  auto it2 = st.find(23);
+  st.erase(it1, it2);
+  st.erase(st.begin(), st.end());
+}
+
+
+// unordered Set : similar to set, but the only thing is it is not sorted.
+void unordered_set_example(){
+  unordered_set<int> s;
+  s.insert(3);
+  s.insert(4);
+  s.insert(1);
+  s.insert(2);
+  
+}
+
+
+// Map
+void map_example(){
+  map<int, int> mp;
+  mp[1] = 3;
+  mp.emplace(3, 8);
+  mp.insert({4, 2});
+
+}
+
+
+// unordered_Map
+void unordered_map_example(){
+  // only thing is, it is not sorted.
+  unordered_map<int,int> mp;
+  mp.insert({2,3});
+}
+
+// custom comparater
+
+bool comp(pair<int,int> p1, pair<int,int> p2){
+  if(p1.second < p2.second) return true;
+  if(p1.second > p2.second) return false;
+
+  if(p1.first > p2.first) return true;
+  else return false;
+  // this function helps to sort in increasing order of second element and if two second elements are same then it sorts in decreasing order with reference to the first element.
+}
+
+void algo(){
+  int n =7;
+  int cnt = __builtin_popcount(n);
+  string s="123";
+  cout<<next_permutation(s.begin(), s.end());
+
+}
+
+int main(){
+  return 0;
+}

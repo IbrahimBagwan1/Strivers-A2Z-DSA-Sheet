d1ec2c07411e3d30d0aba50fc67799dc9d57add3
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-11 20:11:40 +0530

    updated code

---
+++ Step 1 Learn the basics/reverse_int.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int reverse = 0;
+ 
+        while(x!=0){
+            int last = x%10;
+            x/=10;
+            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && last > 7)) return 0;
+            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && last < -8)) return 0;
+            reverse = reverse *10 + last;
+        }
+        return reverse;
+    }
+};

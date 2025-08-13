331138ff5ab2cf1b48843da0f0dfd85a44685730
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-13 11:56:23 +0530

    updated code

--- Step 2 Learn Important Sorting Techniques/sorting 2/merge_sort.cpp
+++ Step 2 Learn Important Sorting Techniques/sorting 2/merge_sort.cpp
@@ -1,28 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define Size 100
 
+void merge(int arr[],int lo, int mid, int hi){
+  int i,j,k;
+  i = lo;
+  j = mid+1;
+  k = lo;
+  int brr[Size];
+  while(i<=mid && j<=hi){
+    if(arr[i] < arr[j]){
+      brr[k++] = arr[i++];
+    }
+    else{
+      brr[k++] = arr[j++];
+    }
+  }
+  
+  while(i<=mid){
+    brr[k++] = arr[i++];
+  }
+  
+  while(j<= hi){
+    brr[k++] = arr[j++];
+  }
 
-void merge(int arr[],int lo, int hi){
-
+  for(int i=lo; i<=hi; i++){
+    arr[i] = brr[i];
+  }
 }
 
 void merge_sort(int arr[], int lo, int hi){
   if(lo<hi){
-    int mid = lo + lo-hi/2;
+    int mid = lo + (hi-lo)/2;
 
     merge_sort(arr, lo, mid);
     merge_sort(arr, mid+1, hi);
 
-    merge(arr, lo, hi);
+    merge(arr, lo, mid, hi);
   }
 }
 
 
 int main(){
-  int arr[] = {2,3,5,1,9,8,7,6,4};
+  int arr[] = {2,44,22,3,5,1,9,8,7,6,4};
   // int n = 9;
   int n = sizeof(arr)/ sizeof(arr[0]);
-  merge_sort(arr, 0, n);
+  merge_sort(arr, 0, n-1);
   for(int ele: arr){
     cout<< ele<< " ";
   }
---
+++ Step 2 Learn Important Sorting Techniques/sorting 2/quick_sort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+void quick_sort(int arr[]){
+  
+}
+
+int main(){
+
+  return 0;
+}

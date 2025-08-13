31fe79afecfd5334a23c33a62ab87cb287afefb6
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-13 12:32:01 +0530

    updated code

---
+++ Step 3 Solve Problems on Arrays/1. easy/second_largest_arr.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ArrayOperations {
+private:
+  vector<int> arr;
+public:
+  void inputArray(int n) {
+    arr.resize(n);
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; ++i) {
+      cin >> arr[i];
+    }
+  }
+
+  // You write the core logic here
+  int findLargest() {
+    int n = arr.size();
+    int first =-1;
+    int second =0;
+    for(int i=0; i<n; i++){
+      if(arr[i] > first){
+        second = first;
+        first = arr[i];
+      }
+    }
+    return second;
+  }
+
+  void secondDisplayLargest() {
+    int largest = findLargest();
+    cout << "The Second Largest element in the array: " << largest << endl;
+  }
+};
+
+int main() {
+  int n;
+  cout << "Enter size of array: ";
+  cin >> n;
+
+  ArrayOperations ao;
+  ao.inputArray(n);
+  ao.secondDisplayLargest();
+
+  return 0;
+}

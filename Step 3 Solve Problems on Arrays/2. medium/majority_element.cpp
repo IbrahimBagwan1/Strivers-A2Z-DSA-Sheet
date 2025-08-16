f3cd4f91518c0af060542c93ffd7154276a0e9ea
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-16 15:19:10 +0530

    updated code

--- Step 3 Solve Problems on Arrays/2. medium/majority_element.cpp
+++ Step 3 Solve Problems on Arrays/2. medium/majority_element.cpp
@@ -4,23 +4,51 @@ using namespace std;
 
 class Solution {
 public:
-  int majorityElement(vector<int>& nums) {
-    int el = nums[0];
-    int count = 1;
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int count1=0, count2=0, el1=INT_MIN, el2=INT_MIN;
+        for(int i=0; i<n; i++){
 
-    for (int i = 1; i < nums.size(); i++) {
-      if (count == 0) el = nums[i];
+            if(count1==0 && nums[i] != el2){
+                count1=1;
+                el1=nums[i];
+            }
+            else if(count2==0 && nums[i] != el1){
+                count2=1;
+                el2=nums[i];
+            }
+            else if(nums[i] == el1) count1++;
+            else if(nums[i] == el2) count2++;
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        vector<int> ans;
+        count1=0, count2=0;
+        for(int i=0; i<n ;i++){
+            if(nums[i]==el1) count1++;
+            else if(nums[i]==el2) count2++;
+        }
 
-      if (nums[i] == el) count++;
-      else count--;
+        if(count1 > n/3){
+            ans.push_back(el1);
+        }
+        if(count2 > n/3){
+            ans.push_back(el2);
+        }
+        return ans;
     }
-    return el;
-  }
 };
 
 int main() {
   Solution sol;
   vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
-  cout << "Majority Element: " << sol.majorityElement(nums) << endl;
+  vector<int> result = sol.majorityElement(nums);
+  cout << "Majority Element: ";
+  for (int el : result) {
+      cout << el << " ";
+  }
+  cout << endl;
   return 0;
 }

d296a981014ba3e638291cd82a527798241e7d01
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-24 11:46:16 +0530

    updated code

--- Step 5 Strings/basic & easy string problems/largest_odd_num_string.cpp
+++ Step 5 Strings/basic & easy string problems/largest_odd_num_string.cpp
@@ -1,14 +1,32 @@
-#include<string>
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     string largestOddNumber(string num) {
         int n = num.length();
-        int idx=-1;
-        for(int i=n-1; i>=0 ;i--){
-            if((num[i]-'0') % 2 == 1  ){ 
-                return num.substr(0, i+1);
+        for (int i = n - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1) {  // check if odd
+                return num.substr(0, i + 1);
             }
         }
         return "";
     }
 };
+
+int main() {
+    Solution sol;
+
+    // Hardcoded inputs
+    string num1 = "35420";
+    string num2 = "4206";
+
+    cout << "Input: " << num1 << " -> Largest odd number: " 
+         << sol.largestOddNumber(num1) << endl;
+
+    cout << "Input: " << num2 << " -> Largest odd number: " 
+         << sol.largestOddNumber(num2) << endl;
+
+    return 0;
+}

42987a36c8e61ccd3d2341d65148389e58b954e8
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-08-29 07:53:01 +0530

    updated code

--- Step 7 Recursion PatternWise/2. Subsequences Pattern/power_set.cpp
+++ Step 7 Recursion PatternWise/2. Subsequences Pattern/power_set.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // Also known as : Power Set
 class Solution {
 public:
-    vector<string> helperGenerateString(int n, string s, int last){
+    vector<string> helperGenerateString(int n, string s){
         vector<string> ans;
         if(n==0){
             ans.push_back(s);
@@ -11,21 +11,21 @@ public:
         }
 
         // collect results from adding '0'
-        vector<string> part1 = helperGenerateString(n-1, s+'0', 0);
+        vector<string> part1 = helperGenerateString(n-1, s+'0');
         ans.insert(ans.end(), part1.begin(), part1.end());
 
-        // collect results from adding '1' (only if last == 0)
-        if(last == 0) {
-            vector<string> part2 = helperGenerateString(n-1, s+'1', 1);
-            ans.insert(ans.end(), part2.begin(), part2.end());
-        }
+        // collect results from adding '1'
+        
+        vector<string> part2 = helperGenerateString(n-1, s+'1');
+        ans.insert(ans.end(), part2.begin(), part2.end());
+
 
         return ans;
     }
 
     vector<string> generateBinaryStrings(int n) {
         vector<string> ans;
-        ans = helperGenerateString(n, "", 0);
+        ans = helperGenerateString(n, "");
         return ans;
     }
 };
@@ -36,7 +36,7 @@ int main() {
     vector<int> testCases = {2, 3, 4, 5, 6}; // 5 hardcoded inputs
 
     for(int n : testCases){
-        cout << "Binary strings of length " << n << " without consecutive 1s:\n";
+        cout << "Binary strings of length " << n << ":\n";
         vector<string> res = s.generateBinaryStrings(n);
         for(auto &str : res){
             cout << str << " ";

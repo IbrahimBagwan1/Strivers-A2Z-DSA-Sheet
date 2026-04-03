ec8777056a35adbd35636383db9ea5f9e21934e8
IbrahimBagwan1 <irbagwan12@gmail.com>
2026-04-03 10:13:08 +0530

    updated code

--- Step 15 Graphs/1. Learning/dfs_bfs.cpp
+++ Step 15 Graphs/1. Learning/dfs_bfs.cpp
@@ -54,6 +54,27 @@ public:
         }
         return bfs;
     }
+
+    vector<int> bfsOfGraphPractice(int V, vector<int> adj[]){
+        vector<int> bfs;
+        queue<int> q;
+        vector<int> visited(V, 0);
+        q.push(0);
+        visited[0]=1;
+
+        while(!q.empty()){
+            int node = q.front(); q.pop();
+            bfs.push_back(node);
+
+            for(auto it: adj[node]){
+                if(!visited[it]){
+                    visited[it]=1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
 };
 
 int main() {

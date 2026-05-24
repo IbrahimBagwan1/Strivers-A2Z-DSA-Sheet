859c75cdcdcbf2280e15853db7b05ab9edcad1a8
IbrahimBagwan1 <irbagwan12@gmail.com>
2026-05-24 20:20:56 +0530

    updated code

--- Step 11 Heaps/2. Medium Problems/find_kth_largest.cpp
+++ Step 11 Heaps/2. Medium Problems/find_kth_largest.cpp
@@ -14,6 +14,18 @@ public:
         }
         return k_max;
     }
+
+    int findKthLargest2(vector<int>& nums, int k){
+        int n = nums.size();
+        priority_queue<int, vector<int> , greater<int>> pq;
+
+        for(int i=0; i<n; i++){
+            pq.push(nums[i]);
+            if(pq.size() > k) pq.pop();    
+        }
+
+        return pq.top();
+    }
 };
 
 int main() {

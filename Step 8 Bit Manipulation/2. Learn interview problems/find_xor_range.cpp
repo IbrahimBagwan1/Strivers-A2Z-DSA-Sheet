74571daaeba1bff312436d92269530ae91843060
IbrahimBagwan1 <irbagwan12@gmail.com>
2026-08-04 07:33:14 +0530

    updated code

--- Step 8 Bit Manipulation/2. Learn interview problems/find_xor_range.cpp
+++ Step 8 Bit Manipulation/2. Learn interview problems/find_xor_range.cpp
@@ -7,6 +7,8 @@ public:
         if (n % 4 == 0) return n;
         if (n % 4 == 1) return 1;
         if (n % 4 == 2) return n + 1;
+        if (n % 4 == 3) return 0;
+        
         return 0;
     }
 
